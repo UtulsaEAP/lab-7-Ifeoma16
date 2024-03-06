ee9b8cfc9d0ca38323e8be53548c010739a28e4d
Ifeoma16 <ifeomalockwood@gmail.com>
2024-03-06 12:29:48 -0600

    problem 1

--- Problem 1/exceptionHandling.py
+++ Problem 1/exceptionHandling.py
@@ -1,16 +1,28 @@
+'''
+Name: Ifeoma Ogwu
+Lab Time: Thursday, 2pm - 3:15pm
+'''
+
 def exceptionHandling():
-    # Split input into 2 parts: name and age
-    parts = input().split()
-    name = parts[0]
-    while name != '-1':
-        # FIXME: The following line will throw ValueError exception.
-        #        Insert try/except blocks to catch the exception.
-        age = int(parts[1]) + 1
-        print(f'{name} {age}')
-        
-        # Get next line
+    try:
+        # Split input into 2 parts: name and age
         parts = input().split()
         name = parts[0]
+        while name != '-1':
+            try:
+                # FIXME: The following line will throw ValueError exception.
+                #        Insert try/except blocks to catch the exception.
+                age = int(parts[1]) + 1
+                print(f'{name} {age}')
+            except ValueError:
+                print(f'{name} 0')
+                
+            # Get next line
+            parts = input().split()
+            name = parts[0]
+    
+    except IndexError:
+        print()
 
 if __name__ == '__main__':
     exceptionHandling()
