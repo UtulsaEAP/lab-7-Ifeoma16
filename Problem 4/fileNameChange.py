4867bb4d8e3d793875336daddca4cb4a1cd22ee0
Ifeoma16 <ifeomalockwood@gmail.com>
2024-03-06 14:42:04 -0600

    problem 4

--- Problem 4/fileNameChange.py
+++ Problem 4/fileNameChange.py
@@ -1,5 +1,19 @@
+'''
+Name: Ifeoma Ogwu
+Lab Time: Thursday, 2pm - 3:15pm
+'''
+
 def fileNameChange():
     #type your code here
+    input_file = input()
+    try:
+        with open(input_file, 'r') as file:
+            for line in file:
+                line = line.strip()
+                modified_line = line.replace("_photo.jpg", "_info.txt")
+                print(modified_line)
+    except FileNotFoundError:
+        return
 
     return
 
