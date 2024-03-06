6213a37bba74c62a71dc0cc8017a80d80a930c31
Ifeoma16 <ifeomalockwood@gmail.com>
2024-03-06 12:36:32 -0600

    problem 2

--- Problem 2/wordInRange.py
+++ Problem 2/wordInRange.py
@@ -1,5 +1,27 @@
+'''
+Name: Ifeoma Ogwu
+Lab Time: Thursday, 2pm - 3:15pm
+'''
+
 def wordInRange():
     #Type your code here
+    file_name = input()
+    lower_bounds = input()
+    upper_bounds = input()
+
+    try:
+        with open(file_name, 'r') as file:
+            lines = file.readlines()
+        
+        for word in lines:
+            word = word.strip()
+            if word >= lower_bounds and word <= upper_bounds:
+                print(f'{word} - in range')
+            else:
+                print(f'{word} - not in range')
+    
+    except FileNotFoundError:
+        return
     return
 if __name__ == '__main__':
     wordInRange()
