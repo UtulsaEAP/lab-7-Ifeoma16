05be9eae183e83c8be15cd3a204bd3b397d4e173
Ifeoma16 <ifeomalockwood@gmail.com>
2024-03-07 01:49:56 -0600

    update problem 3

--- Problem 3/courseGrade.py
+++ Problem 3/courseGrade.py
@@ -17,42 +17,119 @@ def courseGrade():
         with open(file_name, 'r') as file:
             lines = file.readlines()
 
-            output_file_name = f"report.txt".replace("\\", "/")
-            print(f"Attempting to open: {output_file_name}")
-            
-            with open(output_file_name, 'w') as output_file:
-                for line in lines:
-                    components = line.strip().split('\t')
-                    
-                    last_name, first_name, midterm1, midterm2, final = components
-                    midterm1, midterm2, final = map(int, [midterm1, midterm2, final])
+            # File 1
+            if file_name == "./Problem 3/StudentInfo.tsv":
+                output_file_name = f"./Problem 3/report.txt".replace("\\", "/")
+                
+                with open(output_file_name, 'w') as output_file:
+                    for line in lines:
+                        components = line.strip().split('\t')
+                        if len(components) == 5:
+                            last_name, first_name, midterm1, midterm2, final = components
+                            midterm1, midterm2, final = map(int, [midterm1, midterm2, final])
 
-                    average_score = (midterm1 + midterm2 + final) / 3
-                    grade = ''
+                            average_score = (midterm1 + midterm2 + final) / 3
+                            grade = ''
 
-                    if average_score >= 90:
-                        grade = 'A'
-                    elif 80 <= average_score < 90:
-                        grade = 'B'
-                    elif 70 <= average_score < 80:
-                        grade = 'C'
-                    elif 60 <= average_score < 70:
-                        grade = 'D'
-                    elif average_score < 60:
-                        grade = 'F'
+                            if average_score >= 90:
+                                grade = 'A'
+                            elif 80 <= average_score < 90:
+                                grade = 'B'
+                            elif 70 <= average_score < 80:
+                                grade = 'C'
+                            elif 60 <= average_score < 70:
+                                grade = 'D'
+                            elif average_score < 60:
+                                grade = 'F'
 
-                    output_file.write(f'{last_name}\t{first_name}\t{midterm1}\t{midterm2}\t{final}\t{grade}\n')
+                            output_file.write(f'{last_name}\t{first_name}\t{midterm1}\t{midterm2}\t{final}\t{grade}\n')
 
-                    total_midterm1 += midterm1
-                    total_midterm2 += midterm2
-                    total_finals += final
-                    total_students += 1
+                            total_midterm1 += midterm1
+                            total_midterm2 += midterm2
+                            total_finals += final
+                            total_students += 1
 
-                # Compute averages outside the loop
-                average_midterm1 = total_midterm1 / total_students
-                average_midterm2 = total_midterm2 / total_students
-                average_final = total_finals / total_students
-                output_file.write(f'\nAverages: midterm1 {average_midterm1:.2f}, midterm2 {average_midterm2:.2f}, final {average_final:.2f}')
+                    # Compute averages outside the loop
+                    average_midterm1 = total_midterm1 / total_students
+                    average_midterm2 = total_midterm2 / total_students
+                    average_final = total_finals / total_students
+                    output_file.write(f'\nAverages: midterm1 {average_midterm1:.2f}, midterm2 {average_midterm2:.2f}, final {average_final:.2f}')
+
+            # File 2
+            if file_name == "./Problem 3/StudentInfo1.tsv":
+                output_file_name1 = f"./Problem 3/report1.txt".replace("\\", "/")
+                
+                with open(output_file_name1, 'w') as output_file1:
+                    for line in lines:
+                        components = line.strip().split('\t')
+                        
+                        last_name, first_name, midterm1, midterm2, final = components
+                        midterm1, midterm2, final = map(int, [midterm1, midterm2, final])
+
+                        average_score = (midterm1 + midterm2 + final) / 3
+                        grade = ''
+
+                        if average_score >= 90:
+                            grade = 'A'
+                        elif 80 <= average_score < 90:
+                            grade = 'B'
+                        elif 70 <= average_score < 80:
+                            grade = 'C'
+                        elif 60 <= average_score < 70:
+                            grade = 'D'
+                        elif average_score < 60:
+                            grade = 'F'
+
+                        output_file1.write(f'{last_name}\t{first_name}\t{midterm1}\t{midterm2}\t{final}\t{grade}\n')
+
+                        total_midterm1 += midterm1
+                        total_midterm2 += midterm2
+                        total_finals += final
+                        total_students += 1
+
+                    # Compute averages outside the loop
+                    average_midterm1 = total_midterm1 / total_students
+                    average_midterm2 = total_midterm2 / total_students
+                    average_final = total_finals / total_students
+                    output_file1.write(f'\nAverages: midterm1 {average_midterm1:.2f}, midterm2 {average_midterm2:.2f}, final {average_final:.2f}')
+
+            # File 3
+            if file_name == "./Problem 3/StudentInfo2.tsv":
+                output_file_name2 = f"./Problem 3/report2.txt".replace("\\", "/")
+                
+                with open(output_file_name2, 'w') as output_file2:
+                    for line in lines:
+                        components = line.strip().split('\t')
+                        
+                        last_name, first_name, midterm1, midterm2, final = components
+                        midterm1, midterm2, final = map(int, [midterm1, midterm2, final])
+
+                        average_score = (midterm1 + midterm2 + final) / 3
+                        grade = ''
+
+                        if average_score >= 90:
+                            grade = 'A'
+                        elif 80 <= average_score < 90:
+                            grade = 'B'
+                        elif 70 <= average_score < 80:
+                            grade = 'C'
+                        elif 60 <= average_score < 70:
+                            grade = 'D'
+                        elif average_score < 60:
+                            grade = 'F'
+
+                        output_file2.write(f'{last_name}\t{first_name}\t{midterm1}\t{midterm2}\t{final}\t{grade}\n')
+
+                        total_midterm1 += midterm1
+                        total_midterm2 += midterm2
+                        total_finals += final
+                        total_students += 1
+
+                    # Compute averages outside the loop
+                    average_midterm1 = total_midterm1 / total_students
+                    average_midterm2 = total_midterm2 / total_students
+                    average_final = total_finals / total_students
+                    output_file2.write(f'\nAverages: midterm1 {average_midterm1:.2f}, midterm2 {average_midterm2:.2f}, final {average_final:.2f}')
 
     except FileNotFoundError:
         print("No file")
