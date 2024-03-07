6178104f4796063e18cf9ecff8f1d3eff99dc15b
Ifeoma16 <ifeomalockwood@gmail.com>
2024-03-07 00:15:19 -0600

    update problem 3

--- Problem 3/courseGrade.py
+++ Problem 3/courseGrade.py
@@ -4,29 +4,27 @@ Lab Time: Thursday, 2pm - 3:15pm
 '''
 
 def courseGrade():
-    
     # TODO: Declare any necessary variables here. 
-      total_students = 0
-      total_midterm1 = 0
-      total_midterm2 = 0
-      total_finals = 0
-      
-    # TODO: Read a file name from the user and read the tsv file here. 
-      file_name = input()
+    total_students = 0
+    total_midterm1 = 0
+    total_midterm2 = 0
+    total_finals = 0
 
-      try:
-           with open(file_name, 'r') as file:
-                lines = file.readlines()
+    # TODO: Read a file name from the user and read the tsv file here. 
+    file_name = input()
+
+    try:
+        with open(file_name, 'r') as file:
+            lines = file.readlines()
+
+            output_file_name = f"report.txt".replace("\\", "/")
+            print(f"Attempting to open: {output_file_name}")
             
-                output_file_name = f"{file_name.split('.')[0]}.tsv".replace("\\", "/")
-                print(f"Attempting to open: {output_file_name}")
-                output_file = open(output_file_name, 'w')
-                
+            with open(output_file_name, 'w') as output_file:
                 for line in lines:
                     components = line.strip().split('\t')
                     
                     last_name, first_name, midterm1, midterm2, final = components
-
                     midterm1, midterm2, final = map(int, [midterm1, midterm2, final])
 
                     average_score = (midterm1 + midterm2 + final) / 3
@@ -34,11 +32,11 @@ def courseGrade():
 
                     if average_score >= 90:
                         grade = 'A'
-                    elif average_score < 90 and average_score >= 80:
+                    elif 80 <= average_score < 90:
                         grade = 'B'
-                    elif average_score < 80 and average_score >= 70:
+                    elif 70 <= average_score < 80:
                         grade = 'C'
-                    elif average_score < 70 and average_score >= 60:
+                    elif 60 <= average_score < 70:
                         grade = 'D'
                     elif average_score < 60:
                         grade = 'F'
@@ -50,17 +48,16 @@ def courseGrade():
                     total_finals += final
                     total_students += 1
 
-    # TODO: Compute student grades and exam averages, then output results to a text file here.
-                    average_midterm1 = total_midterm1 / total_students
-                    average_midterm2 = total_midterm2 / total_students
-                    average_final = total_finals / total_students
-                    output_file.write(f'\nAverages: midterm1 {average_midterm1:.2f}, midterm2 {average_midterm2:.2f}, final {average_final:.2f}')
-                    output_file.close()
-   
-      except FileNotFoundError:
-          print("No file")
-      except OSError as e:
-          print(f"error opening file: {e}")
+                # Compute averages outside the loop
+                average_midterm1 = total_midterm1 / total_students
+                average_midterm2 = total_midterm2 / total_students
+                average_final = total_finals / total_students
+                output_file.write(f'\nAverages: midterm1 {average_midterm1:.2f}, midterm2 {average_midterm2:.2f}, final {average_final:.2f}')
+
+    except FileNotFoundError:
+        print("No file")
+    except OSError as e:
+        print(f"Error opening file: {e}")
 
 
 
